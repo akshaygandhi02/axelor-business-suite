aff8a33c50fea0eee382d5d7c7ffab7f32fa3bb3
Akshay Gandhi <a.gandhi@axelor.com>
2021-06-30 11:27:41 +0530

    Replace old studio editor with new studio editor

---
+++ axelor-studio/src/main/webapp/studio/editor/precache-manifest.dfeacc62ed24074a3c9fe8b78e9045ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7e06c5f780839085131df710f0462c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a9985183265043ca3cf",
+    "url": "./static/css/main.92f2694a.chunk.css"
+  },
+  {
+    "revision": "fff48067f8f5a34e4240",
+    "url": "./static/js/2.0dfbdbbd.chunk.js"
+  },
+  {
+    "revision": "7c566d33ee52b0fdd07cf1e1fc55662c",
+    "url": "./static/js/2.0dfbdbbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9985183265043ca3cf",
+    "url": "./static/js/main.ba7f6c2e.chunk.js"
+  },
+  {
+    "revision": "194ab46e6f8f75ded56d",
+    "url": "./static/js/runtime-main.637a0453.js"
+  }
+]);
